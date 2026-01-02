27142f8bcac8dcadaaa737428aa2cb32dcb7e290
GeekyProgrammer07 <sumansamantaiitkgp@gmail.com>
2026-01-02 16:12:48 +0530

    add connection pooling with r2d2 across apps

--- packages/store/src/error.rs
+++ packages/store/src/error.rs
@@ -1,10 +1,11 @@
-use diesel::{ConnectionError, result::Error};
+use diesel::{ConnectionError, r2d2, result::Error};
 
 #[derive(Debug)]
 pub enum StoreError {
     Env(dotenvy::Error),
     Db(ConnectionError),
     Res(Error),
+    Pool(r2d2::PoolError),
 }
 
 impl From<dotenvy::Error> for StoreError {
@@ -22,5 +23,11 @@ impl From<ConnectionError> for StoreError {
 impl From<Error> for StoreError {
     fn from(e: Error) -> Self {
         StoreError::Res(e)
+    }
+}
+
+impl From<r2d2::PoolError> for StoreError {
+    fn from(e: r2d2::PoolError) -> Self {
+        StoreError::Pool(e)
     }
 }
--- packages/store/src/models/user.rs
+++ packages/store/src/models/user.rs
@@ -1,7 +1,7 @@
 use crate::{error::StoreError, store::Store};
 use chrono::NaiveDateTime;
 use diesel::{
-    RunQueryDsl, Selectable, SelectableHelper,
+    PgConnection, RunQueryDsl, Selectable, SelectableHelper,
     dsl::insert_into,
     prelude::{Insertable, Queryable},
 };
@@ -9,7 +9,7 @@ use serde::Deserialize;
 use uuid::Uuid;
 
 #[derive(Queryable, Selectable, Debug)] // This rust type exactly mathches on row of the user table
-#[diesel(table_name = crate::schema::user)] // Links thisstruct to user table type in schema.rs
+#[diesel(table_name = crate::schema::user)] // Links this struct to user table type in schema.rs
 #[diesel(check_for_backend(diesel::pg::Pg))] // Ensures DB colum type == Rust field Type and errors at compile time
 // TODO: add explicit getters to see the pass and username as they are private
 pub struct User {
@@ -31,13 +31,13 @@ pub struct NewUser {
 }
 
 impl Store {
-    pub fn create_user(&mut self, new_user: NewUser) -> Result<User, StoreError> {
+    pub fn create_user(conn: &mut PgConnection, new_user: NewUser) -> Result<User, StoreError> {
         use crate::schema::user;
 
         let user = insert_into(user::table)
             .values(&new_user)
             .returning(User::as_returning())
-            .get_result(&mut self.conn)?;
+            .get_result(conn)?;
 
         Ok(user)
     }
