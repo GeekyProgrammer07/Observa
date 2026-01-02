5515c5e5feb0308ebe3056f456b4591d57cc709c
GeekyProgrammer07 <sumansamantaiitkgp@gmail.com>
2026-01-02 16:12:12 +0530

    add connection pooling with r2d2 across apps

--- packages/store/src/store.rs
+++ packages/store/src/store.rs
@@ -1,15 +1,22 @@
-use diesel::{Connection, PgConnection};
+use diesel::{
+    PgConnection,
+    r2d2::{ConnectionManager, Pool},
+};
 
 use crate::{config::Config, error::StoreError};
 
+pub type PgPool = Pool<ConnectionManager<PgConnection>>;
+
+#[derive(Debug, Clone)]
 pub struct Store {
-    pub conn: PgConnection,
+    pub pool: PgPool,
 }
 
 impl Store {
-    pub fn connect() -> Result<Self, StoreError> {
+    pub fn new() -> Result<Self, StoreError> {
         let database_url = Config::from_env()?.db_url;
-        let connection = PgConnection::establish(&database_url)?;
-        Ok(Self { conn: connection })
+        let manager = ConnectionManager::<PgConnection>::new(database_url);
+        let pool = Pool::builder().max_size(10).build(manager)?;
+        Ok(Self { pool })
     }
 }
